d286eacbd54cd0534e976383ee2e4358033e844a
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-24 13:24:06 -0400

    Add logout

--- auth/validate.js
+++ auth/validate.js
@@ -1,23 +1,7 @@
-const bcrypt = require('bcryptjs');
-const User = require('../models/user.js');
-
 module.exports = async function(req, res, next) {
-  const { name, password } = req.headers;
-
-  if (name && password) {
-    try {
-      const user = await User.findBy({ name }).first();
-
-      if (user && bcrypt.compareSync(password, user.password)) {
-        next();
-      } else {
-        res.status(401).json({message: "Invalid credentials"});
-      }
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({message: "Error logging in"});
-    }
+  if (req.session && req.session.user) {
+    next();
   } else {
-    res.status(500).json({message: 'No credentials'});
+    res.status(400).json({message: 'No credentials provided'});
   }
 }
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const session = require('express-session');
 const server = express();
 const bcrypt = require('bcryptjs');
+
 const User = require('./models/user.js');
 const validate = require('./auth/validate.js');
 
+const sessionOptions = {
+  name: "mycookie",
+  secret: "Nel mezzo del cammin di nostra vita",
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false, // true in production
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+}
+
 server.use(express.json());
+server.use(session(sessionOptions));
 
 server.get('/', (req, res) => {
   res.status(200).send('<h1>Welcome to the Users API</h1>');
@@ -40,6 +55,7 @@ server.post('/api/login', async (req, res) => {
     const user = await User.findBy({ name }).first();
 
     if (user && bcrypt.compareSync(password, user.password)) {
+      req.session.user = user;
       res.status(200).json({...user, message: `Welcome ${user.name}`});
     } else {
       res.status(401).json({message: "Invalid credentials"});
@@ -47,6 +63,20 @@ server.post('/api/login', async (req, res) => {
   } catch (err) {
     console.log(err);
     res.status(500).json({message: "Error logging in"});
+  }
+});
+
+server.delete('/api/logout', (req, res) => {
+  if (req.session) {
+    req.session.destroy(err => {
+      if (err) {
+        res.status(500).json({message: "Unable to logout"});
+      } else {
+        res.status(200).json({message: "Logout successful"})
+      }
+    })
+  } else {
+    res.end();
   }
 });
 
